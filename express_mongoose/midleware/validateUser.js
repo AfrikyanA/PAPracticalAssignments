35e724e71d744f9b174da1fd905f97d39ec10d5d
AfrikyanA <afrikyan_1995@mail.ru>
2024-08-27 12:20:20 +0400

    several changes

--- express_mongoose/midleware/validateUser.js
+++ express_mongoose/midleware/validateUser.js
@@ -2,7 +2,9 @@ import { body, validationResult} from 'express-validator';
 
 export const validateUser = (name, email, age) => {
     return [
-        body('name').isAlpha().withMessage('Name must contains only letters (a-zA-Z).').isLength({ min: 3}).withMessage('Name must have >3 characters'),
+        body('name')
+            .isAlpha().withMessage('Name must contains only letters (a-zA-Z).')
+            .isLength({ min: 3}).withMessage('Name must have >3 characters'),
         body('email').isEmail().withMessage('Invalid email address'),
         body('age').isInt({ min: 0, max: 120 }).withMessage('Age must be a number between 0 and 120.'),
     
